c3adccccf794b56fe3461d32611fb23800560ed3
Ritesh560 <75880449+Ritesh560@users.noreply.github.com>
2021-12-23 23:42:42 +0530

    Add files via upload

---
+++ chat-app/src/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+//components
+import Join from "./components/Join/Join"
+import Chat from "./components/Chat/Chat"
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Join />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
