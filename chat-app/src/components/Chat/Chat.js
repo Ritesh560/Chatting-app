7878403192c0557b25beac938abc87852298e621
Ritesh Kumar <singlaritesh007@gmail.com>
2023-05-06 21:23:45 +0530

    feat: enviornments

--- chat-app/src/components/Chat/Chat.js
+++ chat-app/src/components/Chat/Chat.js
@@ -19,7 +19,7 @@ const Chat = () => {
   const [users, setUsers] = useState("")
   const [message, setMessage] = useState("")
   const [messages, setMessages] = useState([])
-  const ENDPOINT = "https://react-socke-chat-app.herokuapp.com/"
+  const ENDPOINT = process.env.REACT_APP_BACKEND_URL
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search)
